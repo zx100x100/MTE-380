2f242b15d49d83cf247bf61d40563e540387727c
Ahmad <1ahmadkamal1@gmail.com>
2022-02-02 11:39:00 -0500

    Created struct for position to be passed from navigation to guidance

---
+++ software/mte380_main/position.h
@@ -0,0 +1,43 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+#include <stdint.h>
+
+
+typedef struct {
+  union {
+    float location[3];
+    struct {
+      float x_loc;
+      float y_loc;
+      float z_loc;
+    };
+  };
+  
+  union {
+    float velocity[3];
+    struct {
+      float x_velocity;
+      float y_velocity;
+      float z_velocity;
+    };
+  };
+  
+  union {
+    float accel[3];
+    struct {
+      float x_accel;
+      float y_accel;
+      float z_accel;
+    };
+  };
+  
+  union {
+    float rotation[3];
+    struct {
+      float xy_rot;  // will not be modulus-ed
+      float xz_rot;
+      float yz_rot;
+    };
+  };
+} position_t;
