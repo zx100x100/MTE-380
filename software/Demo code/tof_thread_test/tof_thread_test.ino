2781e3857f9d9089a3d677ae32a2d83e541a1aa5
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2022-03-31 01:26:35 -0400

    nut

--- software/Demo code/tof_thread_test/tof_thread_test.ino
+++ software/Demo code/tof_thread_test/tof_thread_test.ino
@@ -37,136 +37,141 @@ volatile TofData tofData;
 
 void setup()
 {
-    Serial.begin(115200);
-    delay(5000);
+  Serial.begin(115200);
+  delay(5000);
 
-    Wire.begin();
-    Wire.setClock(400000);
+  Wire.begin();
+  Wire.setClock(400000);
 
-//     Serial.print("sensor Ptr: "); Serial.printf("%lu\n", (void*)&sensor_vl53lx_sat[0]);
+//   Serial.print("sensor Ptr: "); Serial.printf("%lu\n", (void*)&sensor_vl53lx_sat[0]);
 
-     xSemaphore = xSemaphoreCreateBinary();
-        xSemaphoreGive(xSemaphore);
+  xSemaphore = xSemaphoreCreateBinary();
+  xSemaphoreGive(xSemaphore);
 
 
 
   for (int i = 0; i < 4; ++i){
-    pinMode(tofPins[i], OUTPUT);
-    digitalWrite(tofPins[i], LOW);
-    tofInfo[i] = {&sensor_vl53lx_sat[i], {0, 0, 0, 0}, i};
+  pinMode(tofPins[i], OUTPUT);
+  digitalWrite(tofPins[i], LOW);
+  tofInfo[i] = {&sensor_vl53lx_sat[i], {0, 0, 0, 0}, i};
   }
 
-    Serial.println("Started");
+  Serial.println("Started");
+  xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
+  xTaskCreate(
+    watchdog, /* Task function. */
+    "watchdog: ", /* name of task. */
+    1000, /* Stack size of task */
+    NULL, /* parameter of the task */
+    2, /* priority of the task */
+    NULL
+  ); /* Task handle to keep track of created task */
+  xSemaphoreGive(xSemaphore);
+  Serial.println("blah1");
+  for (int i = 0; i < 4; i++){
     xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
-        xTaskCreate(
-            watchdog, /* Task function. */
-            "watchdog: ", /* name of task. */
-            1000, /* Stack size of task */
-            NULL, /* parameter of the task */
-            2, /* priority of the task */
-            NULL
-        ); /* Task handle to keep track of created task */
+    xTaskCreate(
+      tofTask, /* Task function. */
+      "tof Task: ", /* name of task. */
+      5000, /* Stack size of task */
+      &tofInfo[i], /* parameter of the task */
+      1, /* priority of the task */
+      &xHandle[i]
+    ); /* Task handle to keep track of created task */
+    Serial.printf("tof%d giving back semaphore\n", i);
     xSemaphoreGive(xSemaphore);
-    Serial.println("blah1");
-    for (int i = 0; i < 4; i++){
-        xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
-        xTaskCreate(
-            tofTask, /* Task function. */
-            "tof Task: ", /* name of task. */
-            5000, /* Stack size of task */
-            &tofInfo[i], /* parameter of the task */
-            1, /* priority of the task */
-            &xHandle[i]
-        ); /* Task handle to keep track of created task */
-        Serial.printf("tof%d giving back semaphore\n", i);
-        xSemaphoreGive(xSemaphore);
-        delay(500);
-        //delay(250);
+    delay(500);
+    //delay(250);
 
-    }
+  }
 }
 
 void loop()
 {
-    delay(1000);
-        Serial.println("setting high power");
-    motors.setPower(200,200);
-    delay(1000);
-    Serial.println("setting 0 power");
-    motors.setPower(0, 0);
+  delay(1000);
+    Serial.println("setting high power");
+  motors.setPower(200,200);
+  delay(1000);
+  Serial.println("setting 0 power");
+  motors.setPower(0, 0);
 
 }
 
 /* the forever loop() function is invoked by Arduino ESP32 loopTask */
 void watchdog(void * param)
 {
-    while(true){
-        delay(1000);
-        Serial.println("this is ESP32 Task");
-        for (int i = 0; i < 4; ++i){
-            if (xHandle[i] == NULL){
-                vTaskDelay(100);
-                continue;
-            }
-            if ((micros() - lastReading[i])/1000 > 500){
+  unsigned long lastReinit = 0;
+  unsigned long dontReinitUntilThisNumberOfMsPasses = 100;
+  while(true){
+    delay(1000);
+    Serial.println("this is ESP32 Task");
+    for (int i = 0; i < 4; ++i){
+      if (xHandle[i] == NULL){
+        vTaskDelay(100);
+        continue;
+      }
+      micros() - lastReinit > dontReinitUntilThisNumberOfMsPasses * 1000
+      if ((micros() - lastReading[i])/1000 > 500{
+        lastReinit = micros();
 
-                Serial.printf("Watchdog go brrrrrrrr on %d\n", i);
-                vTaskDelete(xHandle[i]);
-                Serial.printf("We killed it %d\n", i);
-                xSemaphoreGive(xSemaphore); // TODO: make this check if already taken
+        Serial.printf("Watchdog go brrrrrrrr on %d\n", i);
+        vTaskDelete(xHandle[i]);
+        Serial.printf("We killed it %d\n", i);
+        xSemaphoreGive(xSemaphore); // TODO: make this check if already taken
 
 
-                Serial.printf("about to create task after like 5 sec%d\n", i);
-                delay(5000);
-                Serial.printf("about to create task rn%d\n", i);
-                xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
-                xTaskCreate(
-                    tofTask, /* Task function. */
-                    "tof Task: ", /* name of task. */
-                    5000, /* Stack size of task */
-                    &tofInfo[i], /* parameter of the task */
-                    1, /* priority of the task */
-                    &xHandle[i]
-                ); /* Task handle to keep track of created task */
-                Serial.printf("made another task %d\n", i);
-                xSemaphoreGive(xSemaphore);
+        Serial.printf("about to create task after like 5 sec%d\n", i);
+        /* delay(5000); // TODO make instant */
+        /* Serial.printf("about to create task rn%d\n", i); */
+        xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
+        Serial.println("got semi fore mal");
+        xTaskCreate(
+          tofTask, /* Task function. */
+          "tof Task: ", /* name of task. */
+          5000, /* Stack size of task */
+          &tofInfo[i], /* parameter of the task */
+          1, /* priority of the task */
+          &xHandle[i]
+        ); /* Task handle to keep track of created task */
+        Serial.printf("made another task %d\n", i);
+        xSemaphoreGive(xSemaphore);
 
-            }
-        }
+      }
     }
+  }
 }
 
 /* this function will be invoked when additionalTask was created */
 void tofTask( void * tofInfoPtr )
 {
-    Wire.begin();
+  Wire.begin();
 
-    TofInfo* currTofInfoPtr = static_cast<TofInfo*>(tofInfoPtr);
+  TofInfo* currTofInfoPtr = static_cast<TofInfo*>(tofInfoPtr);
+  xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
+  tof[currTofInfoPtr->tofIndex] = Tof(currTofInfoPtr->sensor, currTofInfoPtr->tofIndex);
+  xSemaphoreGive(xSemaphore);
+  Serial.println("Initialized");
+
+  while(millis() < START_DELAY);
+  delay(10);
+  /* tof.print = false; */
+  Serial.println("Started");
+  /* loop forever */
+  for(;;){
+    lastReading[currTofInfoPtr->tofIndex] = micros();
+
     xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
-    tof[currTofInfoPtr->tofIndex] = Tof(currTofInfoPtr->sensor, currTofInfoPtr->tofIndex);
-    xSemaphoreGive(xSemaphore);
-    Serial.println("Initialized");
-
-    while(millis() < START_DELAY);
-    delay(10);
-    /* tof.print = false; */
-    Serial.println("Started");
-    /* loop forever */
-    for(;;){
-        lastReading[currTofInfoPtr->tofIndex] = micros();
-
-        xSemaphoreTake(xSemaphore, MUTEX_TIMEOUT);
-        Serial.printf("tof%d: poll: %lu\n", currTofInfoPtr->tofIndex, lastReading[currTofInfoPtr->tofIndex]);
-        tof[currTofInfoPtr->tofIndex].poll();
-        if (tof[currTofInfoPtr->tofIndex].getData().count != prevCount){
-            Serial.printf("tof%d: Count: %d, Dist: %d, numObjs: %d, timeoutCount: %d\n",
-            currTofInfoPtr->tofIndex, tof[currTofInfoPtr->tofIndex].getData().count, tof[currTofInfoPtr->tofIndex].getData().dist, tof[currTofInfoPtr->tofIndex].getData().numObjs, tof[currTofInfoPtr->tofIndex].getData().timeoutCount);
-            prevCount = tof[currTofInfoPtr->tofIndex].getData().count;
-        }
-        xSemaphoreGive(xSemaphore);
-        delay(5);
+    Serial.printf("tof%d: poll: %lu\n", currTofInfoPtr->tofIndex, lastReading[currTofInfoPtr->tofIndex]);
+    tof[currTofInfoPtr->tofIndex].poll();
+    if (tof[currTofInfoPtr->tofIndex].getData().count != prevCount){
+      Serial.printf("tof%d: Count: %d, Dist: %d, numObjs: %d, timeoutCount: %d\n",
+      currTofInfoPtr->tofIndex, tof[currTofInfoPtr->tofIndex].getData().count, tof[currTofInfoPtr->tofIndex].getData().dist, tof[currTofInfoPtr->tofIndex].getData().numObjs, tof[currTofInfoPtr->tofIndex].getData().timeoutCount);
+      prevCount = tof[currTofInfoPtr->tofIndex].getData().count;
     }
-    /* delete a task when finish,
-    this will never happen because this is infinity loop */
-    vTaskDelete( NULL );
+    xSemaphoreGive(xSemaphore);
+    delay(5);
+  }
+  /* delete a task when finish,
+  this will never happen because this is infinity loop */
+  vTaskDelete( NULL );
 }
